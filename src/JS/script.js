24ef045b77b1745950382066a06dffe22d5b73be
Daniil <tyumencev.daniil@mail.ru>
2020-06-29 23:04:56 +0800

    third commit with check savings

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,5 +1,15 @@
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+
+start();
 
 let appData = {
     budget: money,
@@ -7,18 +17,26 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = prompt("Во сколько это обойдется?");
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце"),
+            b = prompt("Во сколько это обойдется?");
 
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
+        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        }
     }
-};
+}
+
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+
 
 appData.moneyPerDay = appData.budget / 30;
 
@@ -33,3 +51,15 @@ if (appData.moneyPerDay < 100) {
 } else {
     console.log("произошла ошибка");
 };
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+    }
+}
+checkSavings();
